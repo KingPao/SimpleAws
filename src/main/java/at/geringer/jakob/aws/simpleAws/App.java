3b87db969d2c4cd873556127d80973b582dfb181
JakobGeringer <jakob.geringer@accenture.com>
2019-05-21 09:53:02 +0200

    init repo

---
+++ src/main/java/at/geringer/jakob/aws/simpleAws/App.java
@@ -0,0 +1,13 @@
+package at.geringer.jakob.aws.simpleAws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
